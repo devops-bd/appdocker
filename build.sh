0a7547ac891453883e33bfc597b5a71a7b246a07
Amran <smamran.com@gmail.com>
2025-10-01 16:02:05 +0600

    dos2unix build.sh converted

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Stop and remove containers, networks, volumes
-docker-compose down
+docker compose down
 
 # Rebuild containers without cache
-docker-compose build --no-cache
+docker compose build --no-cache
 
 # Start containers in detached mode
-docker-compose up -d
+docker compose up -d
